bc39d97e06a14548d09123e9ff841ffe012c3bc0
Jungho Kim <jeonghogim318@gmail.com>
2025-05-10 12:31:14 +0900

    docs: config 적용설정

---
+++ src/test/java/com/example/rebookuserservice/RebookUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rebookuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebookUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
