3928764550abd0870b7a6f0028518b817ef564ca
Jungho Kim <jeonghogim318@gmail.com>
2025-05-14 17:19:30 +0900

    feat: 토큰생성구현

--- src/main/java/com/example/rebookgateway/utils/JwtUtil.java
+++ src/main/java/com/example/rebookgateway/utils/JwtUtil.java
@@ -1,11 +1,57 @@
 package com.example.rebookgateway.utils;
 
-import com.example.rebookgateway.model.UserInfo;
-import lombok.RequiredArgsConstructor;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
+
+    private final SecretKey key;
+    private final long accessValidity;
+    private final long refreshValidity;
+
+    public JwtUtil(
+        @Value("${jwt.secret}") String key,
+        @Value("${jwt.access-expiration}") long accessTokenExpiration,
+        @Value("${jwt.refresh-expiration}") long refreshTokenExpiration
+    ) {
+        this.key = Keys.hmacShaKeyFor(key.getBytes());
+        this.accessValidity = accessTokenExpiration;
+        this.refreshValidity = refreshTokenExpiration;
+    }
+
+    public String createAccessToken(String subject, long memberId) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + accessValidity);
+
+        return Jwts.builder()
+            .subject(subject)
+            .claim("memberId", memberId)
+            .issuedAt(now)
+            .expiration(validity)
+            .signWith(key, Jwts.SIG.HS512) // 0.12.x 버전의 새로운 서명 방식
+            .compact();
+    }
+
+    public String createRefreshToken(String subject, long memberId) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + refreshValidity);
+
+        return Jwts.builder()
+            .subject(subject)
+            .claim("memberId", memberId)
+            .issuedAt(now)
+            .expiration(validity)
+            .signWith(key, Jwts.SIG.HS512) // 0.12.x 버전의 새로운 서명 방식
+            .compact();
+    }
+
+
 
 
 }
