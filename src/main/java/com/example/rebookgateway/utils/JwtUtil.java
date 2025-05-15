0d73d4745fee89eac8d6b06a707f74dd6ef9f434
Jungho Kim <jeonghogim318@gmail.com>
2025-05-15 09:41:56 +0900

    feat: jwtUtil 구현

--- src/main/java/com/example/rebookgateway/utils/JwtUtil.java
+++ src/main/java/com/example/rebookgateway/utils/JwtUtil.java
@@ -25,33 +25,47 @@ public class JwtUtil {
         this.refreshValidity = refreshTokenExpiration;
     }
 
-    public String createAccessToken(String subject, long memberId) {
+    public String createAccessToken(String userId, String username) {
         Date now = new Date();
         Date validity = new Date(now.getTime() + accessValidity);
 
         return Jwts.builder()
-            .subject(subject)
-            .claim("memberId", memberId)
+            .subject(userId)
+            .claim("username", username)
             .issuedAt(now)
             .expiration(validity)
             .signWith(key, Jwts.SIG.HS512) // 0.12.x 버전의 새로운 서명 방식
             .compact();
     }
 
-    public String createRefreshToken(String subject, long memberId) {
+    public String createRefreshToken(String userId) {
         Date now = new Date();
         Date validity = new Date(now.getTime() + refreshValidity);
 
         return Jwts.builder()
-            .subject(subject)
-            .claim("memberId", memberId)
+            .subject(userId)
             .issuedAt(now)
             .expiration(validity)
             .signWith(key, Jwts.SIG.HS512) // 0.12.x 버전의 새로운 서명 방식
             .compact();
     }
 
+    public String getUserId(String token){
+        return Jwts.parser()
+            .verifyWith(key)   // 0.12.x 버전의 새로운 검증 방식, 유요한지?, 만료되었는지
+            .build()
+            .parseSignedClaims(token)
+            .getPayload()
+            .getSubject();
+    }
 
-
+    public String getUsername(String token){
+        return Jwts.parser()
+            .verifyWith(key)   // 0.12.x 버전의 새로운 검증 방식, 유요한지?, 만료되었는지
+            .build()
+            .parseSignedClaims(token)
+            .getPayload()
+            .get("username", String.class);
+    }
 
 }
